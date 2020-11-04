b071bee8272f603ddeb9eeaeb1135507ba3d9e3c
cmiller <cmiller@graphiclanguage.net>
2020-11-04 11:10:06 -0500

    fetch wines and populate select menu with wine names

--- front-end/src/components/WineSelect.js
+++ front-end/src/components/WineSelect.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import { Select, Box, MenuItem } from '@material-ui/core';
 
 export default function WineSelect() {
     const [state, setState] = React.useState({
-        wine: 'None',
+        selectedWine: 'None',
+        wines: [],
     });
 
     const handleChange = (event) => {
         setState({
-            wine: event.target.value,
+            ...state,
+            selectedWine: event.target.value,
         });
     };
+
+    const fetchWines = () => {
+        fetch('http://localhost:3000/wines/')
+            .then((response) => response.json())
+            .then((data) => setState({
+                ...state,
+                wines: data,
+            }));
+    };
+
+    useEffect(() => {
+        fetchWines();
+    }, []);
 
     return (
         <Box textAlign="center" bgcolor="primary.main">
             <FormControl>
                 <Select
-                    value={state.wine}
+                    value={state.selectedWine}
                     onChange={handleChange}
                     inputProps={{ 'aria-label': 'Select Wine' }}
                 >
                     <MenuItem value="None">
                         None
                     </MenuItem>
-                    <MenuItem value="value">value</MenuItem>
+                    {state.wines.map((wine) => (
+                        <MenuItem key={wine.id} value={wine.name}>{wine.name}</MenuItem>
+                    ))}
                 </Select>
             </FormControl>
         </Box>
